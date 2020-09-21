a58f925eee1274fb24de3e8a68360d1d89a27cf2
imadartist <madisonleyens@gmail.com>
2020-09-21 00:48:10 -0500

    worked on markovand added comments

--- src/MarkovGenerator.java
+++ src/MarkovGenerator.java
@@ -1,4 +1,14 @@
+/*
+ * Programmer: Madison Leyens
+ * Class: MelodyPlayer
+ * Date: 9.20.2020
+ * Description: Generating melodies not just based on probability of occurrence of a given note but also using the probability of 
+ * the note appearing at a given position in the sequence of notes
+ */
+
 import java.util.ArrayList;
+
+//Markov Generator will use ProbabilityGenerator to get the counts and probabilities
 
 public class MarkovGenerator<T> extends ProbabilityGenerator<T> {
 
@@ -9,6 +19,7 @@ public class MarkovGenerator<T> extends ProbabilityGenerator<T> {
 	void train(ArrayList<T> newTokens) {
 		
 	}
+	
 	ArrayList<T> generate( int length ) 
 	{
 		ArrayList<T> newSequence = new ArrayList<T>();
@@ -28,4 +39,23 @@ public class MarkovGenerator<T> extends ProbabilityGenerator<T> {
 //		}
 		return newSequence;
 	}
+	/*
+	 * ArrayList<ArrayList<Integer>> transitionTable( ArrayList alphabet<int input>
+	 * ); { 
+	 * to create the ArrayList 
+	 * transitionTable = new ArrayList();
+	 * 
+	 * to add a row 
+	 * ArrayList<Integer> myRow = new ArrayList(); myRow.add(8);
+	 * 
+	 * to get the 0th (i.e., first) ArrayList or row from our ArrayList of
+	 * Arraylists:
+	 * 
+	 * ArrayList<Integer> row = transitionTable.get(0);
+	 * 
+	 * then to get the 0th element from the row (assuming it is both not empty &
+	 * has been instantiated) Integer myElement = row.get(0);
+	 * 
+	 * System.out.println(row.get(0)); //will print 8. }
+	 */
 }

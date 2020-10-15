341c6157bf58618242f943c5beb624de8c5c4a73
imadartist <madisonleyens@gmail.com>
2020-10-15 15:22:49 -0500

    cleaned up main for all project 1 unit test and the first two unit tests of project 2

--- src/ProjectZeroMIDIMain.java
+++ src/ProjectZeroMIDIMain.java
@@ -32,54 +32,58 @@ public class ProjectZeroMIDIMain extends PApplet {
 
 	//setting the window size to 300x300
 	public void settings() {
-		size(500, 500);
+		size(1000, 1000);
 
 	}
 
 	//doing all the setup stuff
 	public void setup() {
-//		fill(120, 50, 240);
-//		
-//		//create my generators for pitch and rhythm
-//		ProbabilityGenerator<Integer>pitchGenerator =  new ProbabilityGenerator<Integer>();
-//		ProbabilityGenerator<Double>rhythmGenerator =  new ProbabilityGenerator<Double>();
-//		
-//		//MarkovGenerator<Integer>pitchMarkovGenerator =  new MarkovGenerator<Integer>();
-//		MarkovGenerator<Double>rhythmMarkovGenerator =  new MarkovGenerator<Double>();
-//		
-//		// returns a url
-//		String filePath = getPath("mid/gardel_por.mid");
-//		
-//		// playMidiFile(filePath);
-//		midiNotes = new MidiFileToNotes(filePath); //creates a new MidiFileToNotes -- reminder -- ALL objects in Java must 
-//													//be created with "new". Note how every object is a pointer or reference. Every. single. one.
-//
-//
-////		// which line to read in --> this object only reads one line (or ie, voice or ie, one instrument)'s worth of data from the file
-//		midiNotes.setWhichLine(0);
-//		
-//		//training the generators for pitch and rhythm to get the pitch and rhythm arrays from the MIDI Notes object 
-//		pitchGenerator.train(midiNotes.getPitchArray());
-//		rhythmGenerator.train(midiNotes.getRhythmArray());
-//		
-//		//enables use to generate and play a MIDI sequence file
-//		player = new MelodyPlayer(this, 100.0f);
-//		player.setup();
-//		player.setMelody( pitchGenerator.generate(20) );
-//		player.setRhythm( rhythmGenerator.generate(20) );
-//		player.setMelody( pitchMarkovGenerator.generate(20) );
-//		player.setRhythm( rhythmMarkovGenerator.generate(20) );
+		fill(120, 50, 240);
+	
+		//create generators for pitch and rhythm
+		ProbabilityGenerator<Integer>pitchGenerator =  new ProbabilityGenerator<Integer>();
+		ProbabilityGenerator<Double>rhythmGenerator =  new ProbabilityGenerator<Double>();
+		
+		MarkovGenerator<Integer>pitchMarkovGenerator =  new MarkovGenerator<Integer>();
+		MarkovGenerator<Double>rhythmMarkovGenerator =  new MarkovGenerator<Double>();
+	
+		// returns a url
+		String filePath = getPath("mid/gardel_por.mid");
+		midiNotes = new MidiFileToNotes(filePath); //creates a new MidiFileToNotes 
+		midiNotes.setWhichLine(0); // which line to read in --> this object only reads one line (or ie, voice or ie, one instrument)'s worth of data from the file
+		
+		//training the generators for pitch and rhythm to get the pitch and rhythm arrays from the MIDI Notes object 
+		pitchGenerator.train(midiNotes.getPitchArray());
+		rhythmGenerator.train(midiNotes.getRhythmArray());
+		
+		pitchMarkovGenerator.train(midiNotes.getPitchArray());
+		rhythmMarkovGenerator.train(midiNotes.getRhythmArray());
+		
+		//enables use to generate and play a MIDI sequence file
+		player = new MelodyPlayer(this, 100.0f);
+		player.setup();
+		player.setMelody( pitchGenerator.generate(20) );
+		player.setRhythm( rhythmGenerator.generate(20) );
+		player.setMelody( pitchMarkovGenerator.generate(20) );
+		player.setRhythm( rhythmMarkovGenerator.generate(20) );
 	}
 
 	public void draw() {
-		//player.play(); //play each note in the sequence -- the player will determine whether is time for a note onset
-		//this is what the user sees that allows them to start the program
 		
 		//start and stop boolean
+		//if (mousePressed == false) {
+		player.play(); //play each note in the sequence -- the player will determine whether is time for a note onset
+		//} else {
+			
+		//}
 		
 		textSize(12);
 		fill(0,102,153);
-		text("Press 1 to start unit test 1, 2 to start unit test 2, 3 to start unit test 3, and 4 to start unit test 4!", width/4, height/2);
+		text("Press 1 to start Project 1 Unit Test 1 (Print Probability Generator Probabilities)", width/4, height/2);
+		text("Press 2 to start Project 1 Unit Test 2 (Generate & Print a 20-Note Melody using Probability Generator Probabilities)", width/4, height/2 + 50);
+		text("Press 3 to start Project 1 Unit Test 3 (Generate & Print 10,000 20-Note Melodies using Probability Generator Probabilities)", width/4, height/2 + 100);
+		text("Press 4 to start Project 2 Unit Test 1 (Print the Markov Generator Transition Tables for the Test Melody)", width/4, height/2 + 150);
+		text("Press 5 to start Project 2 Unit Test 2 (Generate & Print a 20-Note Melody using Test Melody and Markov Generator)", width/4, height/2 +200);
 		
 	}
 
@@ -116,10 +120,6 @@ public class ProjectZeroMIDIMain extends PApplet {
 		ProbabilityGenerator<Integer> generatorPitch = new ProbabilityGenerator(); 
 		ProbabilityGenerator<Double> generatorRhythm = new ProbabilityGenerator();
 		
-		//Project 1 - this class the Markov Generator class delineated by Pitch and Rhythm EXTRA?
-		MarkovGenerator<Integer> markovPitches = new MarkovGenerator();
-		MarkovGenerator<Double> markovRhythms = new MarkovGenerator();
-		
 		//Project 2 - declaring Markov Generator array lists for pitches and rhythms
 		MarkovGenerator<Integer> mPitches = new MarkovGenerator();
 		MarkovGenerator<Double> mRhythms = new MarkovGenerator();
@@ -138,8 +138,6 @@ public class ProjectZeroMIDIMain extends PApplet {
 		//Project 1 - training the generators for pitch and rhythm to get the pitch and rhythm arrays from the MIDI Notes Mary object 
 		generatorPitch.train(midiNotesMary.getPitchArray());
 		generatorRhythm.train(midiNotesMary.getRhythmArray());
-		
-		//Project 1 - Why do I not train markovPitches and markovRhythms?
 
 		//Project 2 - training the markov generators for pitch and rhythm to get the pitch and rhythm arrays from the MIDI Notes Mary object 
 		mPitches.train(midiNotesMary.getPitchArray());
@@ -148,56 +146,55 @@ public class ProjectZeroMIDIMain extends PApplet {
 		//Project 3- training the Order M markov generators for pitch and rhythm to get the pitch and rhythm arrays from the MIDI Notes Mary object 
 		pitchesOrderM.train(midiNotesMary.getPitchArray());
 		rhythmsOrderM.train(midiNotesMary.getRhythmArray());
-		System.out.println("here");
 		
 		if (key == ' ') {
 			player.reset();
 			println("Melody started!");
 		} else if( key == '1') { 
-			//runs Project 0 Unit 1 test when the user presses "1" by printing both the pitch and rhythm tokens & probabilities using the methods in ProbabilityGenerator
+			//runs Project 1 Unit Test 1 when the user presses "1" by printing both the pitch and rhythm tokens & probabilities using ProbabilityGenerator
 			generatorPitch.printProbabilityDistribution("Pitches:");
 			generatorRhythm.printProbabilityDistribution("Rhythms:");
 			
 		} else if (key == '2') {
-			//runs Project 0 Unit 2 test when the user presses "2" and calls the generate function to make a melody with 20 notes and then print the pitches and rhythms to the console
+			//runs Project 1 Unit Test 2 test when the user presses "2" and calls the generate function to make a melody with 20 notes and then print the pitch and rhythm tokens & probabilities
 			System.out.println(generatorPitch.generate(20));
 			System.out.println(generatorRhythm.generate(20));
 			
 		} else if (key=='3') {
-			//runs Project 0 Unit 3 test when the user presses "3" and probabilities are calculated and printed for melodies of 20 notes 10,000 times
+			//runs Project 1 Unit Test 3 when the user presses "3" and probabilities are calculated and printed for melodies of 20 notes 10,000 times
+			//creating new probability generators
 			ProbabilityGenerator<Integer> probDistPitch = new ProbabilityGenerator(); 
 			ProbabilityGenerator<Double> probDistRhythm = new ProbabilityGenerator();
-			for (int i = 0; i < 10000; i++) {	
+			for (int i = 0; i < 10000; i++) {	//looping through the following 10000 times
+				//training the new probability generators on the existing generators generating melodies of 20 
 				probDistPitch.train(generatorPitch.generate(20));
 				probDistRhythm.train(generatorRhythm.generate(20));
 			}
-				probDistPitch.printProbabilityDistribution("ProbDist Pitches:");
-				probDistRhythm.printProbabilityDistribution("ProbDist Rhythms:");
+				//printing the pitch and rhythm tokens & probability distributions for 10000 20-note melodies
+				probDistPitch.printProbabilityDistribution("ProbDist Unit Test 3 Pitches:");
+				probDistRhythm.printProbabilityDistribution("ProbDist Unit Test 3 Rhythms:");
 				
 		} else if (key=='4') {
-			//runs unit 4 test when the user presses "4"
-			int initTokenOne = generatorPitch.generate();
-			double initTokenTwo = generatorRhythm.generate();
+			//runs Project 2 Unit Test 1 when the user presses "4" which prints our Markov Generator transition table using Probability Generator initial tokens and Mary Had a Little Lamb
+			int initTokenOne = generatorPitch.generate(); //generating using Probability Generator an initial token to use for pitches
+			double initTokenTwo = generatorRhythm.generate(); //generating using Probability Generator an initial token to use for rhythms
 			
-			mPitches.printTransitionTable("Markov Unit Test 1 Pitches:");
-			mRhythms.printTransitionTable("Markov Unit Test 1 Rhythms:");
+			mPitches.printTransitionTable("Markov Unit Test 1 Pitches:"); //printing the transition table of the Markov Generator pitches using Mary Had a Little Lamb
+			mRhythms.printTransitionTable("Markov Unit Test 1 Rhythms:"); //printing the transition table of the Markov Generator rhythms using Mary Had a Little Lamb
 			
 		} else if (key=='5') {
-			
+			//runs Project 2 Unit Test 2 when the the user presses "5" which generates & prints one 20-note melody from Mary Had a Little Lamb using our Markov Generator
 			System.out.println(mPitches.generate(20));
 			System.out.println(mRhythms.generate(20));
-			
 
 			}
 		}
 }
 //		//} else if (key == '6') {
-			//markovPitches.printTransitionTable("Markov Pitches:");
-//			System.out.println(" ");
-//			markovRhythms.printTransitionTable("Markov Rhythms:");
+		
 //			
 //			mPitches.printTransitionTable("Markov New Melody Pitches:");
-//			mRhythms.printTransitionTable("Markove New Melody Rhythms:");
+//			mRhythms.printTransitionTable("Markov New Melody Rhythms:");
 			//training and generating melodies of 20 notes 10000 times
 //			for (int i = 0; i< 10000; i++)  { 
 //			mPitches.train(generatorPitch.generate(20));

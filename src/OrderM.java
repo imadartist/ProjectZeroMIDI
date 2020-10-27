71341985ca3b43464b57497a0b3484689b4cd6f0
imadartist <madisonleyens@gmail.com>
2020-10-27 10:10:56 -0500

    improvements to Order M train

--- src/OrderM.java
+++ src/OrderM.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.ArrayList;
 
 public class OrderM<T> extends MarkovGenerator<T> {
-	ArrayList<ArrayList<Integer>> transitionTable = new ArrayList(); // declaring the transition table array list which
+	//ArrayList<ArrayList<Integer>> transitionTable = new ArrayList(); // declaring the transition table array list which
 																		// holds an array list of an array list of
 																		// integers
 	MarkovGenerator<T> markovGen = new MarkovGenerator(); // using markov generator class to declare a markov generator
@@ -25,60 +25,51 @@ public class OrderM<T> extends MarkovGenerator<T> {
 	void train(ArrayList<T> newTokens) { // filling the empty transition table
 		for (int i = orderM - 1; i < newTokens.size() - 1; i++) { // loops starting at one less than the sequence size
 																	// for one less than newTokens size
-			
-			
-			ArrayList<T> curSequence = new ArrayList (newTokens.subList(i - (orderM - 1), i)); // create the current sequence of size Order M from the input
-			//adding previous tokens; read about inclusive exclusive...sublists are exclusive
-			System.out.println(curSequence);
-//			// uniqueAlphabetSequences.add(orderM); //add array list of sequences to uniqueAlphabetSequences. do i need this?
-//
-//			//int rowIndex = uniqueAlphabetSequences.indexOf(newTokens.get(i)); //setting rowIndex equal to the indices in newTokens in uniqueAlphabetSequences
-//
-////	if (curSequence == -1) { //if curSequence is not found
-////		int rowIndex = uniqueAlphabetSequences.size(); //TRY ONE setting rowIndex equal to the size of uniqueAlphabetSequences
-//
-//
-////		how do i use rowIndex? i am using it in curSequence, right? 
-//
-////			uniqueAlphabetSequences.add(curSequences);
-//
-////a lot of the below is borrowed from Markov Generator. Would inheritance have been better?
-//
-//			ArrayList<Integer> myRow = new ArrayList(); // create a row array list to add a new row
-//			transitionTable.add(myRow);// add the new row to the transition table
-//			for (int m = 0; m < alphabet.size(); m++) { // new row will start at 0 and loop through all possible
-//														// values in alphabet
-//				myRow.add(0); // add a 0 to myRow array list
-//			}
-//
-//			alphabet.add(newTokens.get(i)); // add the newToken to alphabet
-//			for (int j = 0; j < transitionTable.size(); j++) { // looping through all of the transition table
-//				ArrayList<Integer> row = transitionTable.get(j); // using the new token at myRow to a new column
-//																	// (horizontal expansion)
-//				row.add(0);
-//			}
-////	}
-//
-//			// adding the counts to the transition table
-//			if (lastIndex > -1) { // if we have a token and it is not the first time through
-//				int tokenIndex = alphabet.indexOf(newTokens.get(i + 1));
-//				// if (tokenIndex == -1) { //if tokenIndex is not found in the alphabet
-//				// tokenIndex = alphabet.size();
-//				// alphabet.add(tokenIndex);
-//				// expand the transition table horizontally
-//				// }
-//
-//				// updating the counts
-//				ArrayList<Integer> row = transitionTable.get(lastIndex); // get whatever the lastIndex is in transition
-//																			// table and set it to the row array
-//				Integer transitionColumn = row.get(tokenIndex); // get the row using tokenIndex
-//				Integer transitionRow = row.get(rowIndex);
-//				transitionColumn++;// increment the tokenIndex
-//				transitionRow++;
-//				row.set(tokenIndex, transitionColumn, transitionRow);
-//			}
-//			lastIndex = tokenIndex; // setting current index to previous for next loop
-		}
+
+			ArrayList<T> curSequence = new ArrayList(newTokens.subList(i - (orderM - 1), i + 1)); // create the current
+																									// sequence of size
+																									// Order M from the
+																									// input
+			// adding previous tokens; read about inclusive exclusive...sublists are
+			// exclusive
+
+			int rowIndex = uniqueAlphabetSequences.indexOf(curSequence); // setting rowIndex equal to the indices in
+																			// newTokens in uniqueAlphabetSequences
+
+			if (rowIndex == -1) { // if curSequence is not found
+				rowIndex = uniqueAlphabetSequences.size(); // TRY ONE setting rowIndex equal to the size of
+															// uniqueAlphabetSequences
+				uniqueAlphabetSequences.add(curSequence);
+
+				ArrayList<Integer> myRow = new ArrayList(); // create a row array list to add a new row
+				transitionTable.add(myRow);// add the new row to the transition table
+				for (int m = 0; m < alphabet.size(); m++) { // new row will start at 0 and loop through all possible
+															// values in alphabet
+					myRow.add(0); // add a 0 to myRow array list
+				}
+			}
+
+			int tokenIndex = alphabet.indexOf(newTokens.get(i + 1));
+			if (tokenIndex == -1) {
+				tokenIndex = alphabet.size();
+				alphabet.add(newTokens.get(i + 1)); // add the newToken to alphabet
+				for (int j = 0; j < transitionTable.size(); j++) { // looping through all of the transition table
+					ArrayList<Integer> row = transitionTable.get(j); // using the new token at myRow to a new column																// (horizontal expansion)
+					row.add(0);
+				}
+			}
+
+				// updating the counts
+				ArrayList<Integer> transitionRow = transitionTable.get(rowIndex);														// table and set it to the row array
+				Integer transitionColumn = transitionRow.get(tokenIndex); // get the row using tokenIndex
+
+				transitionColumn++;// increment the tokenIndex
+				transitionRow.set(tokenIndex, transitionColumn);
+		}  
+		System.out.println(uniqueAlphabetSequences);
+		System.out.println(transitionTable);
+//uniquesequences.size instead of alphabet because thats #of rows and alphabet is # of columns
+//have the counts now need to sum and divide to get probabilities
 	}
 }
 
